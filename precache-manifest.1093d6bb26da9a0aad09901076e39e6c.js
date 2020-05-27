26f7bbe2f4e7cc862d9a5d2966e2b8869f6b934f
Nitesh Shrestha <nitesh.shresthax@gmail.com>
2020-05-27 11:28:09 +0545

    Updates

---
+++ precache-manifest.1093d6bb26da9a0aad09901076e39e6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "575f6fe218755c3c36b5cfe8f6fef01b",
+    "url": "/todos-app-react/index.html"
+  },
+  {
+    "revision": "72d67b1f9aa0d4904057",
+    "url": "/todos-app-react/static/css/main.3b011977.chunk.css"
+  },
+  {
+    "revision": "05c1727629a4797f71e2",
+    "url": "/todos-app-react/static/js/2.b3665b24.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todos-app-react/static/js/2.b3665b24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72d67b1f9aa0d4904057",
+    "url": "/todos-app-react/static/js/main.a2ccea16.chunk.js"
+  },
+  {
+    "revision": "f9439d69948fd4db73cc",
+    "url": "/todos-app-react/static/js/runtime-main.9baaf006.js"
+  }
+]);
